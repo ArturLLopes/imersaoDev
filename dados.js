26eedbb3e5656fc6cdbe68dfe82c03bb4d4b3153
ArturLLopes <arturllopes@gmail.com>
2024-09-05 20:16:31 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,17 @@
+let dados = [
+    {
+        titulo: "Rayssa Leal",
+        descricao: "Rayssa Leal, a Fadinha, é uma skatista brasileira que conquistou o mundo com seu talento e carisma. Aos 13 anos, tornou-se a mais jovem medalhista olímpica da história do Brasil.",
+        link: "https://pt.wikipedia.org/wiki/Rayssa_Leal"
+    },
+    {
+        titulo: "Beatriz Souza",
+        descricao: "Beatriz Souza é uma judoca brasileira que se destacou nos Jogos Olímpicos de Paris 2024, conquistando a medalha de ouro. Sua trajetória inspiradora, marcada por superação e dedicação, a tornou um ícone do esporte nacional.",
+        link: "https://pt.wikipedia.org/wiki/Beatriz_Souza"
+    },
+    {
+        titulo: "Rebeca Andrade",
+        descricao: "Rebeca Andrade é uma ginasta brasileira que fez história nos Jogos Olímpicos de Tóquio 2020 e Paris 2024, conquistando diversas medalhas. Sua força, determinação e talento a consolidaram como uma das maiores ginastas do mundo.",
+        link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade"
+    }
+];
